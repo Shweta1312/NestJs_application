f4672b34b8e2c4284228ed51f651c98271523dca
Shweta1312 <shawant11@gmail.com>
2020-11-24 20:16:53 +0530

    Get tasks through filtering/searching

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,10 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
 import { CreateTaskDTo } from './dto/create-task.dto';
+import { GetTasksFilterDTo } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
+
+  getTasksWithFilters(filterDto: GetTasksFilterDTo){
+    const {status, search} = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if(status){
+      tasks = tasks.filter(task => task.status === status);
+    }
+    if(search){
+      tasks = tasks.filter(task =>
+        task.title.includes(search) ||
+        task.description.includes(search),
+      );
+    }
+
+    return tasks;
+  }
 
   getAllTasks(): Task[] {
     return this.tasks;
