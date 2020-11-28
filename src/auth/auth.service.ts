3f21be3ccfa66e4e1505f9f4f5e3ea157356177c
Shweta1312 <shawant11@gmail.com>
2020-11-28 22:25:08 +0530

    Validating password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './auth.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -10,7 +10,15 @@ export class AuthService {
     private userRepository: UserRepository,
   ) {}
 
-  async signUp(authCredentialsDto: AuthCredentialsDto) {
+  async signUp(authCredentialsDto: AuthCredentialsDto):Promise<void> {
     this.userRepository.signUp(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto){
+    const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+    if(!username){
+      throw new UnauthorizedException("Invalid Credentials");
+    }
+    
   }
 }
