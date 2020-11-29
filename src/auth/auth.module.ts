cb28cbfc8de14492282911f67018b30fedfa1407
Shweta1312 <shawant11@gmail.com>
2020-11-29 16:24:38 +0530

    Signing a JWT token on Authentocation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,21 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './auth.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports:[
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: '3600',
+      },
+    }),
     TypeOrmModule.forFeature([UserRepository]),
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
