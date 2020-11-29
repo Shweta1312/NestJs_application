fa2200790436afb24d15595a1f664769374816ab
Shweta1312 <shawant11@gmail.com>
2020-11-29 22:39:24 +0530

    Get user's task by task id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -18,8 +18,8 @@ export class TasksService {
     return this.taskRepository.getTasks(filterDto, user);
   }
 
-  async getTaskById(id: number): Promise<Task> {
-    const found = await this.taskRepository.findOne(id);
+  async getTaskById(id: number, user: User): Promise<Task> {
+    const found = await this.taskRepository.findOne({where: {id, userId: user.id}});
 
     if (!found) {
       throw new NotFoundException(`Task with ID ${id} not found`);
@@ -39,13 +39,13 @@ export class TasksService {
     }
   }
 
-  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
+//   async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+//     const task = await this.getTaskById(id);
 
-    task.status = status;
+//     task.status = status;
 
-    await task.save();
+//     await task.save();
 
-    return task;
-  }
+//     return task;
+//   }
 }
