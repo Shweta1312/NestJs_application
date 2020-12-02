e7c1ef55e03dfc843d8e2117c3df59776cc4e353
Shweta1312 <shawant11@gmail.com>
2020-12-02 13:48:16 +0530

    Unit testing examples using jest

---
+++ src/example.spec.ts
@@ -0,0 +1,61 @@
+//feature
+class FriendsList {
+  friends = [];
+
+  addFriend(name) {
+    this.friends.push(name);
+    this.announceFriendship(name);
+  }
+
+  announceFriendship(name) {
+    console.log(`${name} is now a friend`);
+  }
+
+  removeFriend(name) {
+    const idx = this.friends.indexOf(name);
+    if (idx == -1){
+      throw new Error('Friend not found');
+    }
+
+    this.friends.splice(idx,1);
+  }
+}
+
+//tests
+describe('FriendsList', () => {
+  let friendsList;
+
+  beforeEach(() => {
+    friendsList = new FriendsList();
+  })
+
+  it('initializes friends list', () => {
+    expect(friendsList.friends.length).toEqual(0);
+  });
+
+  it('add a friend to a list', () => {
+    friendsList.addFriend('Sam');
+    expect(friendsList.friends.length).toEqual(1);
+  })
+
+  it('announce friendship', () => {
+    //mock function    
+    friendsList.announceFriendship = jest.fn();
+    expect(friendsList.announceFriendship).not.toHaveBeenCalled();
+    friendsList.addFriend('Ariel');
+    expect(friendsList.announceFriendship).toHaveBeenCalled();
+  })
+
+  describe('removeFriend', () => {
+    it('removes a friend from a list', () => {
+      friendsList.addFriend('Ariel');
+      expect(friendsList.friends[0]).toEqual('Ariel');
+      friendsList.removeFriend('Ariel');
+      expect(friendsList.friends[0]).toBeUndefined();
+    })
+
+    it('throws an error if friend not found', () => {
+      expect(() => friendsList.removeFriend('Sam')).toThrow(new Error('Friend not found')
+    })
+  })
+});
