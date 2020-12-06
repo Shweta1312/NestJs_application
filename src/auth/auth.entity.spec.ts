df1176c502b787757f1fedef845f779803b5787f
Shweta1312 <shawant11@gmail.com>
2020-12-06 23:20:40 +0530

    Trial

--- src/auth/auth.entity.spec.ts
+++ src/auth/auth.entity.spec.ts
@@ -8,24 +8,23 @@ describe('User entity', () => {
     user = new User();
     user.password = 'testPassword';
     user.salt = 'testSalt';
-  })
+  });
 
   describe('validatePassord', () => {
-
     it('returns true as passowrd is valid', async () => {
       bcrypt.hash.mockReturnValue('testPassword');
       expect(bcrypt.hash).not.toHaveBeenCalled();
       const result = await user.validatePassword('1234');
-      expect(bcrypt.hash).toHaveBeenCalledWith('1234','testSalt');
+      expect(bcrypt.hash).toHaveBeenCalledWith('1234', 'testSalt');
       expect(result).toEqual(true);
-    })
+    });
 
     it('returns false as passowrd is invalid', async () => {
       bcrypt.hash.mockReturnValue('wrongPassword');
       expect(bcrypt.hash).not.toHaveBeenCalled();
       const result = await user.validatePassword('12345');
-      expect(bcrypt.hash).toHaveBeenCalledWith('12345','testSalt');
+      expect(bcrypt.hash).toHaveBeenCalledWith('12345', 'testSalt');
       expect(result).toEqual(false);
-    })
-  })
-})
+    });
+  });
+});
